f0165ccf45990cef4ff5af1406e621b45c01d844
EC2 Default User <ec2-user@ip-172-31-86-94.ec2.internal>
2018-12-27 02:10:56 +0000

    Done

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,3 @@ DOCKER_ACCOUNT=sudhakarlanka540
 docker build --tag=customer-service .
 docker tag customer-service $DOCKER_ACCOUNT/customer-service:v1
 docker push $DOCKER_ACCOUNT/customer-service
-
-
